dab4ca6d031241c18b8dd42370ec298bf7ed08ee
DevSoleo <devsoleo@protonmail.com>
2026-01-23 17:54:59 +0100

    feat: dates now easier to read & new column for notification subscriptions

--- src/panel/menus/UsersList.jsx
+++ src/panel/menus/UsersList.jsx
@@ -3,10 +3,11 @@ import Paper from '@mui/material/Paper'
 import { Chip } from '@mui/material'
 import Typography from '@mui/material/Typography'
 import { api } from '../../../axios'
+import dayjs from 'dayjs'
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'uid', headerName: 'UID', width: 170, sortable: false,     renderCell: params => (
+  { field: 'uid', headerName: 'UID', width: 170, sortable: false, renderCell: params => (
       <span style={{ fontFamily: 'monospace' }}>
         {params.value}
       </span>
@@ -27,8 +28,20 @@ const columns = [
       )
     }
   },
-  { field: 'last_login', headerName: 'Dernière connexion', width: 180 },
-  { field: 'creation_date', headerName: 'Date d\'inscription', width: 180 }
+  { field: 'last_login', headerName: 'Dernière connexion', width: 180, renderCell: params => dayjs(params.value).format('HH:mm:ss DD/MM/YYYY') },
+  { field: 'creation_date', headerName: 'Date d\'inscription', width: 180, renderCell: params => dayjs(params.value).format('HH:mm:ss DD/MM/YYYY') },
+  { field: 'notification_token', headerName: 'Inscrit aux notifications', width: 180, renderCell: params => {
+      const isAdmin = params.value
+
+      return (
+        <Chip
+          label={isAdmin ? 'Oui' : 'Non'}
+          color={isAdmin ? 'primary' : 'default'}
+          size="small"
+        />
+      )
+    }
+  }
 ]
 
 let rows = []
